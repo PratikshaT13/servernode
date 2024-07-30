133f7928f352712c2d669169ce9057f7111e7f40
PratikshaT13 <pratikshatigadi@gmail.com>
2024-07-31 03:47:18 +0530

    Add files via upload

---
+++ Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.tracker.bus_tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/","/api/auth/register","/generateQRCode", "/vbl","/all-buses","/vehicles", "/error", "/api/auth/login", "/buses", "/printUsers").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
---
+++ Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.tracker.bus_tracker.controller;
+
+import com.tracker.bus_tracker.entity.AuthUser;
+import com.tracker.bus_tracker.repository.AuthUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000*")
+@RestController
+@AllArgsConstructor
+public class UserController {
+
+    private final AuthUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    @GetMapping("/login")
+    public LoginResponse login(@RequestParam String username, @RequestParam String password) {
+        Optional<AuthUser> userOpt = userRepository.findByUsername(username);
+
+        if (userOpt.isPresent() && passwordEncoder.matches(password,userOpt.get().getPassword())) {
+            return new LoginResponse(true, "Login successful");
+        } else {
+            return new LoginResponse(false, "Invalid username or password");
+        }
+    }
+    @GetMapping ("/register")
+    public LoginResponse registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String phone_no , @RequestParam String email){
+
+        try {
+            if (userRepository.findByUsername(username).isPresent()) {
+                return new LoginResponse(false, "Username already taken. Please try again.");
+            }
+
+            AuthUser user = new AuthUser(username, passwordEncoder.encode(password),phone_no, email);
+            userRepository.save(user);
+            return new LoginResponse(true, "Successfully registered");
+        } catch (Exception e) {
+            return new LoginResponse(false, e.getMessage());
+        }
+    }
+
+
+
+    @GetMapping("/printUsers")
+    public ResponseEntity<List<AuthUser>> printUsers() {
+        List<AuthUser> users = userRepository.findAll();
+        for (AuthUser user : users) {
+            System.out.println("User: " + user.getUsername() + ", Password: " + user.getPassword());
+        }
+        return ResponseEntity.ok(users);
+    }
+}
