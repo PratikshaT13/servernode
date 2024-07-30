764dceff7548e7244ad5ae207744897ee752cc31
PratikshaT13 <pratikshatigadi@gmail.com>
2024-07-31 02:15:20 +0530

    Update HelloController.java

--- Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/controller/HelloController.java
+++ Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sayHello() {
         return "Hello, World!";
     }
