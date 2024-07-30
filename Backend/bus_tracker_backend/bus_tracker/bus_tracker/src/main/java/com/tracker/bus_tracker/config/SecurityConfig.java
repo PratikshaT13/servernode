3fcbee9c7880e14c14ae1f59e9edbadc7741df55
PratikshaT13 <pratikshatigadi@gmail.com>
2024-07-31 04:00:25 +0530

    Update SecurityConfig.java

--- Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/config/SecurityConfig.java
+++ Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://localhost:3000")
+                        .allowedOrigins("https://the-blue-transit.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
