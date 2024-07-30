7761b40ba4273780cc61bb2c6e9cf9e2a25e76dc
PratikshaT13 <pratikshatigadi@gmail.com>
2024-07-31 04:03:32 +0530

    Update UserController.java

--- Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/controller/UserController.java
+++ Backend/bus_tracker_backend/bus_tracker/bus_tracker/src/main/java/com/tracker/bus_tracker/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000*")
+@CrossOrigin(origins = "https://the-blue-transit.vercel.app")
 @RestController
 @AllArgsConstructor
 public class UserController {
